d6deb6c79c4115f3c3517b0b4fe963d5501af7c7
kino0104a <kyousuke.kinoshita@gmail.com>
2018-02-19 09:16:18 +0900

    リファクタリング

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.preprocessing import StandardScaler as SC
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 # データを読み込む
 train = pd.read_csv("train.csv")
@@ -21,9 +23,7 @@ def format_data(data):
 train_data = format_data(train)
 test_data = format_data(test)
 
-model = LR()
-
-
+clf = LR()
 
 # 使用すると特徴
 use_variable = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
@@ -38,14 +38,21 @@ sc = SC()
 train_X_std = sc.fit_transform(train_X)
 test_X_std = sc.fit_transform(test_X)
 
-#
-model.fit(train_X_std, train_y)
+#clf.fit(train_X_std, train_y)
+diparameter = {
+    "C": [10**i for i in range(-10, 20)]
+}
+gsc = GridSearchCV(clf, param_grid=diparameter, cv=3)
+gsc.fit(train_X_new, train_y)
 
-predict = model.predict(test_X_std)
+print(gsc.score(train_X_new,train_y))
+
+#predict = model.predict(test_X_std)
+predict = gsc.predict(test_X_std)
 
 submission = pd.DataFrame({
     "PassengerId": test_data["PassengerId"],
     "Survived": predict
 })
 
-submission.to_csv("submission_std2.csv", index=False)
+submission.to_csv("submission_std_gsc.csv", index=False)
